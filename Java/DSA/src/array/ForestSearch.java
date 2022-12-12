dcffa9a7961f5b18cb2e515c7009d67b004433b9
dipeshsingh253 <sinhdipesh@gmail.com>
2022-12-12 10:50:44 +0530

    dsa day 13

---
+++ Java/DSA/src/array/ForestSearch.java
@@ -0,0 +1,39 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ForestSearch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+
+        for (int i = 0; i < n; i++) {
+
+            arr[i] = scanner.nextInt();
+
+        }
+
+
+        System.out.println(Arrays.toString(arr) + " " + k);
+    }
+
+
+    static int woodCollected(int[] arr, int n, int m){
+        int sum = 0;
+
+
+    }
+
+    public static int findHeight(int[] arr, int n, int k){
+
+
+
+    }
+
+}
